c1ecbb35d6003ed3b412863b02a98b5221d165a9
david hays <purplehays527@gmail.com>
2020-10-30 11:35:53 -0600

    I think everything is set up correctly but I havent tested yet

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -21,6 +21,17 @@ const validateId = (req,res,next) => {
     })
 }
 
+const validateBody = (req,res,next) => {
+    const body = req.body
+
+    if (!body.description || !body.notes){
+        next({code:400, message: "you must have a description and notes"})
+    }else{
+        req.payload = body
+        next()
+    }
+}
+
 router.use((req, res, next) => {
     console.log('inside the action router');
     next();
@@ -29,6 +40,38 @@ router.use((req, res, next) => {
 router.get('/:id',validateId,(req,res) => {
     res.status(200).json(req.action)
 })
+
+router.post('/:id',validateBody, (req,res) => {
+    Action.insert(req.payload)
+    .then(data => {
+        res.status(201).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({message:error.message})
+    })
+})
+
+router.put('/:id',validateId,validateBody,(req,res) => {
+    Action.update(req.params.id,req.body)
+    .then(data => {
+        res.status(201).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({message:"Something went horribly wrong"})
+    })
+})
+
+router.delete('/:id', validateId, (req,res) => {
+    Action.remove(req.params.id)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({message:"Something wen horribly wrong"})
+    })
+})
+
+
 
 router.use((err, req, res, next) => {
     res.status(err.code).json({ message: err.message })
